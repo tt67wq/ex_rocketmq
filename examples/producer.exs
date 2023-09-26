044ca4a29fbe0ac40763c4905d099af8e851d4c0
wanqiang <wanqiang@shizhuang-inc.com>
2023-09-26 14:40:41 +0800

    feat: rename group in example

--- examples/producer.exs
+++ examples/producer.exs
@@ -106,7 +106,7 @@ Supervisor.start_link(
      ]},
     {
       Producer,
-      group_name: "GID_WANQIANG",
+      group_name: "GID_POETRY",
       namesrvs: :namesrvs,
       opts: [
         name: :producer
